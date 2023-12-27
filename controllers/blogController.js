08d6fae0648550955cb71a924e79792ced6be311
ranaadeel15911 <ra0511083@gmail.com>
2023-12-27 23:02:16 +0500

    abc

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -15,22 +15,21 @@ static getAllBlogs = async(req,resp)=>{
 static addNewBlog = async(req,resp)=>{
     // resp.send({message:'hello'})
     const {title,category,description} = req.body
-    // console.log(req.file)
-    // const {filename} = req.file
-    // const result = await cloudinary.uploader.upload(path) 
-
+    console.log(req.file)
+    const {path} = req.file
+    const result = await cloudinary.uploader.upload(path) 
     try {
         if (title && category && description) {
             const addBlog = new blogModel({
                 title:title,
                 description:description,
                 category:category,
-                thumbnail:req.file.filename,
-                // cloudinary_id:result.public_id,
+                thumbnail:result.secure_url,
+                cloudinary_id:result.public_id,
                 user:req.user._id
             })
             const savedBlog = await addBlog.save()
-            // fs.unlinkSync(path)
+            fs.unlinkSync(path)
             if (savedBlog) {
                 resp.status(200).json({message:'Blog added successfully'})
             }
