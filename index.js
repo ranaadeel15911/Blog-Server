14207d5af9d03e04d3ee05390ec830f9dad14a67
ranaadeel15911 <ra0511083@gmail.com>
2023-12-28 16:14:13 +0500

    afja;

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors({
 }))
 app.use(express.json());
 // app.use(express.static("public/upload"))
-app.use(express.static(path.join(__dirname, 'publicc')))
+app.use(express.static(path.join(__dirname + 'publicc')))
 app.get("/",(req,resp)=>{
 resp.send("API is running ...")
 })
