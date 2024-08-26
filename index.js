d40ebdc2c3e9698bb26905f567190a0c1d2d06e4
Rana adeel <112337492+ranaadeel15911@users.noreply.github.com>
2024-08-26 15:02:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,44 @@
-import express from 'express';
-import connectToDb from './config/db.js';
-import cors from 'cors';
-import authRoutes from './routes/blog.js';
-import path from 'path';
-const app = express();
-const PORT = 'https://blog-server-eosin.vercel.app';
-
-connectToDb();
-
-// Use CORS middleware with proper configuration
+import express from 'express'
+import connectToDb from './config/db.js'
+import cors from 'cors'
+import authRoutes from './routes/blog.js'
+import path from 'path'
+const app = express()
+const PORT = 'https://blog-server-eosin.vercel.app'
+// const PORT = 900
+connectToDb()
+// app.use(cors())
 app.use(cors({
-    origin: ["https://blog-frontend-psi-six.vercel.app", "http://localhost:3000"],
-    methods: ["POST", "GET", "DELETE"],
-    credentials: true,
-}));
-
+    origin:["https://blog-frontend-psi-six.vercel.app","http://localhost:3000"],
+    methods:["POST","GET","DELETE"],
+    credentials:true
+}))
 app.use(express.json());
+// app.use(express.static("public/upload"))
+app.use(express.static('publicc'))
+app.get("/",(req,resp)=>{
+resp.send("API is running ...")
+})
+app.use('/api/v1',authRoutes)
+app.listen(PORT,()=>{
+    console.log("API is running on 900 port")
+})
 
-// Serve static files
-app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.get("/", (req, resp) => {
-    resp.send("API is running ...");
-});
-
-app.use('/api/v1', authRoutes);
-
-app.listen(PORT, () => {
-    console.log(`API is running on port ${PORT}`);
+import express from 'express'
+import AuthController from '../controllers/authController.js'
+import BlogController from '../controllers/blogController.js'
+import CategoryControllers from '../controllers/categoryController.js'
+import multer from 'multer'
+import checkIsUserAuthenticated from '../middlewares/authMiddleware.js'
+const router = express.Router()
+                        /* Multer Config */
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, path.resolve('public/upload/'));
+    },
+    filename: function (req, file, cb) {
+        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        cb(null, `${uniqueSuffix}-${file.originalname}`);
+    }
 });
